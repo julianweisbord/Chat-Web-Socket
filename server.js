05f600be755480587bd8ba86ec66549ea963adfb
julianweisbord <julianweisbord@gmail.com>
2015-09-04 08:53:48 -0700

    Init
    Signed-off-by: julianweisbord <julianweisbord@gmail.com>

---
+++ server.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var dt = new Date();
+day_array = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// add a 0 before numbers less than 2
+  minutes = dt.getMinutes();
+
+
+else {
+  minutes = dt.getMinutes();
+}
+
+var time = " said at " + dt.getHours() + ":" + minutes + " on " + day_array[dt.getDay()];
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on("connection", function(socket){
+  socket.on("chat message", function(message){
+    io.emit("chat message", message);
+    io.emit("chat message", time);
+  });
+});
+
+http.listen(8000, function(){
+  console.log("listening on: 8000");
+});
